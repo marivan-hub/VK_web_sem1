88de9b788a4e9478162c2aaf294598ba0a5c3d16
marivan <ivan.maryshev.90@mail.ru>
2025-03-21 18:31:09 +0300

    add base templates for pages

--- Technopark_project/QA_project/views.py
+++ Technopark_project/QA_project/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,"../templates/QA_project/index.html")
+
+def login(request):
+    return render(request,"../templates/QA_project/login.html")
+
+def ask(request):
+    return render(request,"../templates/QA_project/ask.html")
+
+def signup(request):
+    return render(request,"../templates/QA_project/signup.html")
+
+def question(request):
+    return render(request,"../templates/QA_project/question.html")
