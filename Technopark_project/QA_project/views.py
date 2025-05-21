f75f82384ed9f999abe56bc32cd9d7462615b0b9
marivan <vanyamaryshev421@gmail.com>
2025-05-21 18:48:18 +0300

    done with hw 4

--- Technopark_project/QA_project/views.py
+++ Technopark_project/QA_project/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.shortcuts import redirect
+from django.shortcuts import render, get_object_or_404
+
+from .forms import AskForm, AnswerForm
+from .forms import SignupForm, LoginForm, ProfileEditForm
 from .models import Question, Tag, Profile, Answer
+from django.utils.http import url_has_allowed_host_and_scheme
+from django.urls import reverse
+
 
 def paginate(objects_list, request, per_page=10):
     paginator = Paginator(objects_list, per_page)
@@ -50,36 +58,112 @@ def tag(request, tag_name):
 
 def question(request, pk):
     question = get_object_or_404(Question, pk=pk)
-    answers = Answer.objects.filter(question=question).order_by('-is_correct', '-rating')
+    answers_qs = Answer.objects.filter(question=question).order_by('-is_correct', '-rating')
+
     context = get_common_context()
     context['question'] = question
-    context['page'] = paginate(answers, request, per_page=5)
+
+    per_page = 5
+    paginator = Paginator(answers_qs, per_page)
+
+    if request.method == 'POST':
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user.profile
+            answer.question = question
+            answer.save()
+            question.answers_count = question.answers.count()
+            question.save()
+            all_answers = list(answers_qs)
+            answer_index = all_answers.index(answer)
+
+            page_number = answer_index // per_page + 1
+
+            url = reverse('question', kwargs={'pk': pk}) + f'?page={page_number}#answer-{answer.id}'
+            return redirect(url)
+    else:
+        page_number = request.GET.get('page', 1)
+
+    page = paginator.get_page(page_number)
+    context['page'] = page
+    context['form'] = form if request.method == 'POST' else AnswerForm()
+
     return render(request, "QA_project/question.html", context)
 
 
+
+@login_required
 def ask(request):
     context = get_common_context()
+    if request.method == 'POST':
+        form = AskForm(request.POST)
+        if form.is_valid():
+            question = form.save(author=request.user.profile)
+            return redirect('question', pk=question.pk)
+    else:
+        form = AskForm()
+    context['form'] = form
     return render(request, "QA_project/ask.html", context)
 
 
-def login(request):
+def login_view(request):
+    next_url = request.GET.get('continue', request.POST.get('continue', '/'))
     context = get_common_context()
-    return render(request, "QA_project/login.html", context)
+    context['form'] = LoginForm(request.POST or None)
+    context['continue'] = next_url
+    print('tut')
+    if request.method == 'POST' and context['form'].is_valid():
+        print('tut 2')
+        user = authenticate(
+            request,
+            username=context['form'].cleaned_data['username'],
+            password=context['form'].cleaned_data['password']
+        )
+        if user is not None:
+            print('tut 3')
+            login(request, user)
+            print(user)
+            if url_has_allowed_host_and_scheme(next_url, allowed_hosts={request.get_host()}):
+                print("login here")
+                return redirect(next_url)
+            return redirect('index')
+        else:
+            context['form'].add_error(None, 'Invalid username or password')
+
+    return render(request, 'QA_project/login.html', context)
+
+
 
 
 def signup(request):
     context = get_common_context()
+    if request.method == 'POST':
+        form = SignupForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    context['form'] = form
     return render(request, "QA_project/signup.html", context)
 
-
+@login_required
 def settings(request):
     context = get_common_context()
-    context['user'] = request.user.profile
+    profile = request.user.profile
+    if request.method == 'POST':
+        form = ProfileEditForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('settings')
+    else:
+        form = ProfileEditForm(instance=profile)
+    context['form'] = form
     return render(request, "QA_project/settings.html", context)
 
 
-def index_logout(request):
-    questions = Question.objects.new()
-    context = get_common_context()
-    context['page'] = paginate(questions, request)
-    return render(request, "QA_project/index_logout.html", context)
+def logout_view(request):
+    referer = request.META.get('HTTP_REFERER', '/')
+    logout(request)
+    return redirect(referer)
--- Technopark_project/Technopark_project/urls.py
+++ Technopark_project/Technopark_project/urls.py
@@ -1,18 +1,25 @@
 from django.contrib import admin
 from django.urls import path
-from QA_project.views import index, login, ask, signup, question, settings, index_logout, tag, hot
+from QA_project.views import index, login_view, ask, signup, question, settings, tag, hot, logout_view
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', index, name='index'), # главная страница
     path('hot/', hot, name='hot'), # список лучших вопросов
-    path('index_logout/', index_logout, name='index_logout'), # тестовая страничка для демонстрации (потом удалить)
+    # path('index_logout/', index_logout, name='index_logout'), # тестовая страничка для демонстрации (потом удалить)
     path('tag/<str:tag_name>/', tag, name='tag'), # список вопросов по тегу
     path('question/<int:pk>/', question, name='question'), # страница одного вопроса со список ответов
     path('settings/', settings, name='settings'), # страница настроек
-    path('login/', login, name='login'), # форма логина
+    path('login/', login_view, name='login'), # форма логина
     path('signup/', signup, name='signup'), # форма регистрации
     path('ask/', ask, name='ask'), # форма создания вопроса
+    path('logout/', logout_view, name='logout'),
 
 ]
 
+# Для медиа:
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
